1396e179c7dd022a8f6bfa0467f434985f1b73ef
Hasa Elman <elman.hasa@ynap.com>
2019-09-01 21:00:10 +0200

    Update tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,11 +2,28 @@ import Data.Maybe
 import Primes
 import Test.QuickCheck
 
-prop_validPrimesOnly val = if val <0 || val > length primes
+prop_validPrimesOnly val = if val < 2 || val > length primes
                             then result == Nothing
                             else isJust result
     where result = isPrime val
 
+prop_primesArePrime val = if result == Just True
+                            then length divisors == 0
+                            else True
+    where result = isPrime val
+          divisors = filter ((==0) . (val `mod`)) [2 .. (val -1)]
+
+prop_nonPrimesAreComposite val = if result == Just False
+                                    then length divisors > 0
+                                    else True
+    where result = isPrime val
+          divisors = filter ( (==0) . (val `mod`)) [2 .. (val -1)]
+
 main :: IO ()
 main = do
+    putStrLn "Value only for valid input"
     quickCheck prop_validPrimesOnly
+    putStrLn "Are prime"
+    quickCheckWith stdArgs { maxSuccess = 1000} prop_primesArePrime
+    putStrLn "Negative cases are composite"
+    quickCheckWith stdArgs { maxSuccess = 1000} prop_nonPrimesAreComposite
