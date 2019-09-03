063d8aa85a960052f47a19e9898375453bf9ce09
Hasa Elman <elman.hasa@ynap.com>
2019-09-04 00:33:44 +0200

    Print factorials

--- app/Main.hs
+++ app/Main.hs
@@ -2,15 +2,26 @@ module Main where
 
 import Primes
 
+
 toResult :: Maybe Bool -> String
 toResult (Just True) = "It' a prime!"
 toResult (Just False) = "Not a prime"
 toResult Nothing = "Not valid"
 
+factorNumbers :: Int -> String
+factorNumbers n = case result of
+            Nothing -> ""
+            Just (x:[]) ->  ""
+            Just (x:xs) -> "Factorials are " ++ concat( map ((++",") . show) (x:xs))
+            
+    where result = primeFactors n
+
 main :: IO ()
 main = do
     putStrLn "Insert a number to check its primarity" 
     n <- getLine
-    let result = (toResult . isPrime . read) n
+    let value = read n
+    let result = (toResult . isPrime ) value
     putStrLn result
+    putStrLn (factorNumbers value)
     
