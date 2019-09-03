1f712b882f0ce3617167088dfed07ed8db9f9017
Hasa Elman <elman.hasa@ynap.com>
2019-09-03 23:32:24 +0200

    Add main logic

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,15 @@ module Main where
 
 import Primes
 
+toResult :: Maybe Bool -> String
+toResult (Just True) = "It' a prime!"
+toResult (Just False) = "Not a prime"
+toResult Nothing = "Not valid"
+
 main :: IO ()
-main = return ()
+main = do
+    putStrLn "Insert a number to check its primarity" 
+    n <- getLine
+    let result = (toResult . isPrime . read) n
+    putStrLn result
+    
